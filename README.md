df6cf61e63a7f8ceb132cccf9064ea5d8c961e2b
pex <rernst@farbenmeer.net>
2014-12-11 18:45:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Polymer element-slider
+
+This is a very basic responsive element slider that supports **button navigation** and **swiping on mobile devices**.
+
+## Usage
+
+```html
+<element-slider>
+  <img src="/path/to/image1.jpg">
+  <img src="/path/to/image2.jpg">
+  <img src="/path/to/image3.jpg">
+  <div class="map">whatsoever..</div>
+</element-slider>
+```
+
+## Custom Styles
+
+...
+
+## Todos
+
+* Provide information on how to add custom styles
+* Add Demo
+* Add Tests
+
+## Licence
+
+See [LICENCE](MIT-LICENCE). Copyright 2014 Roman Ernst
---
+++ element-slider.js
@@ -0,0 +1,56 @@
+Polymer('element-slider', {
+  counter: 0,
+  touchDown: null,
+
+  domReady: function() {
+    this.children[0].setAttribute('active', '')
+  },
+
+  showCurrent: function() {
+    var item = Math.abs(this.counter % this.children.length);
+    this.eachSlide(function(slide) {
+      slide.removeAttribute('active');
+    });
+    this.children[item].setAttribute('active', '');
+  },
+
+  next: function() {
+    console.log('next');
+    this.counter++;
+    this.showCurrent();
+  },
+
+  prev: function() {
+    console.log('prev');
+    this.counter--;
+    this.showCurrent();
+  },
+
+  initTouch: function(evnt) {
+    this.touchDown = evnt.touches[0];
+  },
+
+  touchMove: function(evnt) {
+    if (!this.touchDown) return;
+
+    var touchDown = this.touchDown,
+    touchUp = evnt.touches[0];
+
+    var diff = {
+      x: touchDown.clientX - touchUp.clientX,
+      y: touchDown.clientY - touchUp.clientY
+    }
+
+    // We're only tracking left/right swiping.
+    if (Math.abs(diff.x) > Math.abs(diff.y)) {
+      if (diff.x > 0) this.next();
+      else this.prev();
+    }
+
+    this.touchDown = null;
+  },
+
+  eachSlide: function(callback) {
+    [].forEach.call(this.children, callback);
+  }
+});
